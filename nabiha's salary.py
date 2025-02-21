4ae2320f1f25bb60df716f7bfbcc5b7fab2591da
fadi50 <adiboughanem45@gmail.com>
2025-02-21 20:34:08 +0200

    this function is made for the user input the percentage's and store them in a dictionary for later use

--- nabiha's salary.py
+++ nabiha's salary.py
@@ -1,5 +1,16 @@
-#
+#this function is used to get the salary details from the user
 def get_salary_details():
     salary = float(input("Enter your salary for the month: ")) 
     month = input("Enter the name of the month: ") 
     return salary, month
+#this function is used to get the percentages of the rent, savings and electricity 
+def get_percentages():
+    savings_percentage = float(input("Enter the percentage of your salary that goes to rent: ")) / 100
+    rent_percentage = float(input("Enter the percentage of your salary that goes to savings: ")) / 100
+    electricity_percentage = float(input("Enter the percentage of your salary that goes to electricity: ")) / 100
+    return {
+        "savings": savings_percentage,
+        "rent": rent_percentage,
+        "electricity": electricity_percentage
+        }
+   #this dictionary is used to store the percentages of the rent, savings and electricity     
